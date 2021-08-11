1a8a5d541e97b8a19ac5528d031944cac63fc250
Zach Bertram <87879943+zachdbertram@users.noreply.github.com>
2021-08-11 12:16:10 -0400

    Update and rename JS to script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function calc(){
 
 MIT License
 
-Copyright (c) [2021] [Dani Crossing, Zach Bertram]
+Copyright (c) [2021] [Dani Crossing]
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
